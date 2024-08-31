98fb7513e6e897fba6ff2a54f773e9f87d8d8051
Alazarb <108571360+Alazarb@users.noreply.github.com>
2024-08-31 18:57:45 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,35 @@
+
+                                      #LOOP
+#i = 0
+#while i < 3:
+    #print("aluka")
+    #i += 1
+                       
+                                  #for loop
+#for i in[0, 1, 2, 3]:
+    #print("hellow, world")
+                        
+
+                                      #calculatorg
+#x = int(input("x: "))
+#y = int(input("y: "))
+#print(x * y)
+                                                                    
+
+var = input("Do you agree  ")
+if var in ["yes", "y"]:
+    print("agree")
+elif var in ["no", "n"]:
+    print("not agree.")
+else:
+    print("say some thing.")
+                                          #win a lottery
+no = int(input("no: "))
+if no in [10]:
+    print("you win first lottary")
+elif no in [5]:
+    print("your you win second lottary")
+elif no in [1]:
+    print("you win therd lottary") 
+else:
+    print("try another time")               
